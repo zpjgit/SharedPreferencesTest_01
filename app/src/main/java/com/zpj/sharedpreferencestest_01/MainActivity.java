fdf4c7d0d62107efbce846f7ae2505a2cdf4d0ad
zpjgit <zpj.love@foxmail.com>
2018-06-06 20:37:23 +0800

    点击写入数据

---
+++ app/src/main/java/com/zpj/sharedpreferencestest_01/MainActivity.java
@@ -0,0 +1,29 @@
+package com.zpj.sharedpreferencestest_01;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button saveData = (Button) findViewById(R.id.save_data);
+
+        saveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {//存数据
+                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+                editor.putString("name", "Tom");
+                editor.putInt("age", 28);
+                editor.putBoolean("married", false);
+                editor.apply();
+            }
+        });
+    }
+}

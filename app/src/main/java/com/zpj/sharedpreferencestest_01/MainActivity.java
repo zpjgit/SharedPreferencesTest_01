664083096a708ce5aa7bdd05c4335ff1ba8df91b
zpjgit <zpj.love@foxmail.com>
2018-06-07 10:05:03 +0800

    点击写入数据 增加读取数据

--- app/src/main/java/com/zpj/sharedpreferencestest_01/MainActivity.java
+++ app/src/main/java/com/zpj/sharedpreferencestest_01/MainActivity.java
@@ -3,8 +3,11 @@ package com.zpj.sharedpreferencestest_01;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.BufferedReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button restorData = (Button) findViewById(R.id.restore_data);
         Button saveData = (Button) findViewById(R.id.save_data);
 
         saveData.setOnClickListener(new View.OnClickListener() {
@@ -23,6 +27,19 @@ public class MainActivity extends AppCompatActivity {
                 editor.putInt("age", 28);
                 editor.putBoolean("married", false);
                 editor.apply();
+            }
+        });
+
+        restorData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+                String name = pref.getString("name", "");
+                int age = pref.getInt("age", 0);
+                boolean married = pref.getBoolean("married", false);
+                Log.d("MainActivity", "name is " + name);
+                Log.d("MainActivity", "age is " + age);
+                Log.d("MainActivity", "married is " + married);
             }
         });
     }
